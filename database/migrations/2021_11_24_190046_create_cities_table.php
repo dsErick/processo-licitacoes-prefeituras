808e5473391352033e0a3914544694020a5d694a
Erick <ericksilvablue@gmail.com>
2021-11-24 19:40:45 -0300

    Model, factory e migration para cidades (cities)

---
+++ database/migrations/2021_11_24_190046_create_cities_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->char('state', 2);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('cities');
+    }
+}
